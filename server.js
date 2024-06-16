b44f5e282cf484e2fbaad0889996b1d12589db13
cx.li <chunxialims@163.com>
2024-06-15 20:13:23 -0700

    simple routing

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import http from 'http';
 
-const PORT = 8000;
+// const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
+    // console.log(req.url);
+    // console.log(req.method);
+    
     // res.write('Hello World!');
     // res.end();
 
@@ -11,11 +15,36 @@ const server = http.createServer((req, res) => {
     // res.statusCode = 404;
     // res.end("<h1>Hello World!!!</h1>")
 
-    res.writeHead(500, {
-        'Content-Type': 'text/json',
-      });
-    res.end(JSON.stringify({message: 'Server Error'}));
-})
+    // res.writeHead(500, {
+    //     'Content-Type': 'text/json',
+    //   });
+    // res.end(JSON.stringify({message: 'Server Error'}));
+
+    try {
+        // Check if GET request
+        if(req.method === 'POST') {
+            // router, routing
+            if(req.url === '/') {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end("<h1>Homepage!!!</h1>");
+            } else if (req.url === '/about') {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end("<h1>About!!!</h1>");
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end("<h1>Not Found!!!</h1>");
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
+        
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/html'});
+        res.end("<h1>Server Error!!!</h1>");
+    }
+    
+
+});
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
